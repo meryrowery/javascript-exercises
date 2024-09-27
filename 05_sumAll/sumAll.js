6c221aa92c56de8c16c4cabd59e85e67397b8f16
meryrowery <m.manikalo@gmail.com>
2024-09-27 15:39:21 +0200

    05 mam dosc

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,25 @@
-const sumAll = function() {
+const sumAll = function(min,max) {
+    if (!Number.isInteger(min) || !Number.isInteger(max)) return "ERROR";
 
+    if (min<0 || max<0) {
+        return "ERROR"
+    }
+
+    if (min > max) {
+        const temp = min;
+        min = max;
+        max = temp;
+      }
+
+    sum = 0
+    for (let i = min;i<max;i++) {
+        sum+=i
+        // console.log(i)
+    }
+    return sum
 };
+
+console.log(sumAll(10,20))
 
 // Do not edit below this line
 module.exports = sumAll;

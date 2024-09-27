9e1321306e8a786fe402cf28e277f248a77ed107
meryrowery <m.manikalo@gmail.com>
2024-09-27 12:22:56 +0200

    03 done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(input) {
+    let reverseInput = ""
+    for (let i=input.length-1;i>=0;i--) {
+        reverseInput += input[i];
+    }
+    return reverseInput
 };
+
+// console.log(reverseString("test"))
 
 // Do not edit below this line
 module.exports = reverseString;

d9737b8cad331c46ffbcbf6298dacc6955d95309
meryrowery <m.manikalo@gmail.com>
2024-09-27 16:03:38 +0200

    07 done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let tempC = (temp - 32) * (5 / 9);
+  return Math.round(tempC * 10) / 10; // Round to one decimal place
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let tempF = temp * (9 / 5) + 32;
+  return Math.round(tempF * 10) / 10; // Round to one decimal place
 };
+
+console.log(convertToCelsius(32))
 
 // Do not edit below this line
 module.exports = {

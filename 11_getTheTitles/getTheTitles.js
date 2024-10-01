22da45c2e4bf20bf04a2cf82051ef315bda526ac
meryrowery <m.manikalo@gmail.com>
2024-10-01 13:43:18 +0200

    11 done

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,23 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (dict) {
+  arr = [];
+  for (i = 0; i < dict.length; i++) {
+    arr.push(dict[i].title);
+  }
+  return arr;
 };
+
+const books = [
+  {
+    title: "Book",
+    author: "Name",
+  },
+  {
+    title: "Book2",
+    author: "Name2",
+  },
+];
+
+console.log(getTheTitles(books));
 
 // Do not edit below this line
 module.exports = getTheTitles;

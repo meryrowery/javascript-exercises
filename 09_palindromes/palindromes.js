6ee58a81ef7e7aaef2ddd67809e8c9fc052ec4d2
meryrowery <m.manikalo@gmail.com>
2024-10-01 13:11:38 +0200

    09 done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str = (str.replace(/[^a-zA-Z]/g, '')).toLowerCase();
+    newStr = "";
+    for (i=str.length-1; i>=0; i--) {
+        newStr += str[i]
+    }
+    return str === newStr
 };
+
+console.log(palindromes("A car, a man, a maraca"))
 
 // Do not edit below this line
 module.exports = palindromes;

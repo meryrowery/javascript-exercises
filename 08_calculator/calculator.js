604ba3dcc8b7664321c47f973c9a6f0b4ffc4e44
meryrowery <m.manikalo@gmail.com>
2024-10-01 10:33:00 +0200

    08 done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,46 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	mappedArr = arr.reduce((total, currentItem) => {
+    return total + currentItem;
+  }, 0);
+  return mappedArr
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+	mappedArr = arr.reduce((total, currentItem) => {
+    return total * currentItem;
+  }, 1);
+  return mappedArr
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return num1 ** num2
 };
 
-const factorial = function() {
-	
+const factorial = function(num1) {
+  if (num1 === 0) return 1
+  
+  
+  const arr = []
+  for (i=1;i<=num1;i++) {
+    arr.push(i)
+  }
+// console.log(arr)  
+mappedArr = arr.reduce((total, currentItem) => {  
+return total * currentItem;
+}, 1);
+return mappedArr
 };
+
+
+console.log(factorial(5))
 
 // Do not edit below this line
 module.exports = {

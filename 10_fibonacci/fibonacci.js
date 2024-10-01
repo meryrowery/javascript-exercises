b3cefc572e1de698b03edc3e5d0d0149b98b2196
meryrowery <m.manikalo@gmail.com>
2024-10-01 13:33:41 +0200

    10 done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function (index) {
+  arr = [1, 1];
+  index = parseInt(index);
+  if (index === 0) {
+    return 0;
+  } else if (index < 0) {
+    return "OOPS";
+  }
 
+  for (i = 2; i < index; i++) {
+    newValue = arr[i - 1] + arr[i - 2];
+    arr.push(newValue);
+  }
+  return arr[i - 1];
 };
+
+console.log(fibonacci(4));
 
 // Do not edit below this line
 module.exports = fibonacci;
